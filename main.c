216304f61f60d0ff8dce8faf7a4577ca05fd98b7
mlavry <taaikiazerolier@gmail.com>
2025-03-26 20:59:30 +0100

    Signal start gestion

--- main.c
+++ main.c
@@ -3,19 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mlavry <mlavry@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mlavry <taaikiazerolier@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 17:55:00 by mlavry            #+#    #+#             */
-/*   Updated: 2025/03/26 18:51:57 by mlavry           ###   ########.fr       */
+/*   Updated: 2025/03/26 20:21:21 by mlavry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	signal_handler(int sig)
+{
+	if (sig == SIGINT)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+}
+
 int main(int argc, char *argv[], char **envp)
 {
+	char	*line;
+
+	(void) argv;
+	(void) envp;
     if (argc != 1)
+	{
         return (0);
-    (void)argv;
-    (void)envp;
+	}
+	signal(SIGINT, signal_handler);
+	signal(SIGQUIT, SIG_IGN);
+	while (1)
+	{
+		line = readline("minishell$ ");
+		if (!line)
+			break;
+		if (*line)
+			add_history(line);
+		free(line);
+	}
+	return (0);
 }

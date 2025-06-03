c7139f9f6ead3d8352d68aa46ff6f79baf35285a
Matis Lavry <mlavry@k1r4p2.42mulhouse.fr>
2025-06-03 15:37:27 +0200

    Special case SHLVL

--- utils_3.c
+++ utils_3.c
@@ -6,7 +6,7 @@
 /*   By: mlavry <mlavry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 23:44:24 by mlavry            #+#    #+#             */
-/*   Updated: 2025/06/02 23:45:06 by mlavry           ###   ########.fr       */
+/*   Updated: 2025/06/03 15:36:23 by mlavry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,16 @@ int	is_numeric(const char *str)
 {
 	int	i;
 
+	i = 0;
 	if (!str || !*str)
 		return (0);
-	if (str[0] == '+' || str[0] == '-')
-		str++;
-	i = 0;
+	while (is_space(str[i]))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
 	while (str[i])
 	{
-		if (!ft_isdigit(str[i]))
+		if (!ft_isdigit(str[i]) && !is_space(str[i]))
 			return (0);
 		i++;
 	}
